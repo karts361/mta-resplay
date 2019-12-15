120c039152090168b4cae24eff59efea7468175c
karts361 <43081466+karts361@users.noreply.github.com>
2019-12-15 19:52:05 +1000

    update

--- mods/deathmatch/resources/[resplay]/att/server.lua
+++ mods/deathmatch/resources/[resplay]/att/server.lua
@@ -2,9 +2,7 @@ function joinHandler(player)
 	if not player then
 		player = source
 	end
-	outputChatBox('Архивный сервер Респлея.', player, 0, 255, 0)
-	outputChatBox('Пожалуйста, не пишите насчёт лидерок или ранга в гос.фракции, все фракции недействующие и вайпнуты.', player, 255, 0, 0)
-	outputChatBox('Правила на сервере также не действуют, но сильно перегибать палку тоже не стоит.', player, 255, 0, 0)
+	outputChatBox('RESPLAY CLOSED BETA.', player, 0, 255, 0)
 	outputChatBox('Клавиши: K - открыть меню, O - меню друзей, I - Инвентарь, L - Скиллы', player, 255, 255, 0)
 end
 addEventHandler('onPlayerJoin', root, joinHandler)
