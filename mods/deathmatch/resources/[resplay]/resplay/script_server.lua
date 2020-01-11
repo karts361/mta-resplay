777dcb289860237f7ba9c17bf04abe75f79877fd
karts361 <43081466+karts361@users.noreply.github.com>
2020-01-11 20:27:41 +1000

    переподключил

--- mods/deathmatch/resources/[resplay]/resplay/script_server.lua
+++ mods/deathmatch/resources/[resplay]/resplay/script_server.lua
@@ -11993,9 +11993,9 @@ function resourceStart(startedResource)
 	
 	repeat
 		--3LcJm524jr
-		db = dbConnect("mysql", "dbname=rsplsrv;host=127.0.0.1;port=3306", "kartos", "Vecmrf12374")
+		--db = dbConnect("mysql", "dbname=rsplsrv;host=127.0.0.1;port=3306", "kartos", "Vecmrf12374")
 		--db = dbConnect("mysql", "dbname=server657169;host=n150.serva4ok.ru;port=3306", "server657169", "gdK9HIuQDE")
-		--db = dbConnect("mysql", "dbname=resplaychik;host=game334530.ourserver.ru;port=3306", "resplaysis", "ebanutogoeliseeva")
+		db = dbConnect("mysql", "dbname=resplaychik;host=game334530.ourserver.ru;port=3306", "resplaysis", "ebanutogoeliseeva")
 	until db
 	
 	loadMapFile()
