7c5e9979893dddb60f8c5f756d91fe3ab045c692
karts361 <43081466+karts361@users.noreply.github.com>
2020-02-29 01:15:36 +1000

    Update script_server.lua

--- mods/deathmatch/resources/[resplay]/resplay/script_server.lua
+++ mods/deathmatch/resources/[resplay]/resplay/script_server.lua
@@ -18807,7 +18807,7 @@ function checkMoney2(dbq, plr, pHash)
 		local curPrice
 		
 		for i=1,table.getn(dbqueryresult) do
-			curPrice = math.ceil(getVehiclePrice(dbqueryresult[i]["carid"])* (dbqueryresult[i]["hp"]/1000)*0.75)
+			curPrice = math.ceil(getVehiclePrice(dbqueryresult[i]["carid"])* (dbqueryresult[i]["hp"]/1000)*0.20)
 			if(curPrice > sellPrice) then
 				sellIndex = i
 				sellPrice = curPrice
