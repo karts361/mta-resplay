e1b5e00a130f3044a14778aa68887532de2cf64e
karts361 <43081466+karts361@users.noreply.github.com>
2020-04-23 22:54:38 +1000

    Update function.lua

--- mods/deathmatch/resources/[resplay]/dirtlevel/server/function.lua
+++ mods/deathmatch/resources/[resplay]/dirtlevel/server/function.lua
@@ -1,6 +1,6 @@
 addEvent("dirt:takemoney", true)
 addEventHandler("dirt:takemoney", getRootElement(),
 	function(money)
-		exports.resplay:takeMoney(source, money)
+		exports.resplay:playerTakeMoney(source, money)
 	end
 )
