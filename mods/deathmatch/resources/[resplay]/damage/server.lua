c946d5f6564504b3ae2e41aff9b9711cd379b3d5
karts361 <43081466+karts361@users.noreply.github.com>
2021-05-09 13:26:44 +1000

    прописал броню пд машинам и урезал дальность тазера

--- mods/deathmatch/resources/[resplay]/damage/server.lua
+++ mods/deathmatch/resources/[resplay]/damage/server.lua
@@ -1,6 +1,8 @@
 function setWeaponStat( ) 
   for _, skill in ipairs({"poor", "std", "pro"}) do 
     setWeaponProperty(38, skill, "damage", 38) 
+    setWeaponProperty(23, skill, "damage", 6) 
+	setWeaponProperty(23, skill, "weapon_range", 14)
     setWeaponProperty( "minigun", skill, "anim_loop_stop", 0.5 )
     setWeaponProperty( "minigun", skill, "anim2_loop_stop", 0.5 )
   end 
--- mods/deathmatch/resources/[resplay]/hummerarmor/client.lua
+++ mods/deathmatch/resources/[resplay]/hummerarmor/client.lua
@@ -22,3 +22,13 @@ function handleHummerDamage(attacker, weapon, loss, x, y, z, tyreID)
 	end
 end
 addEventHandler("onClientVehicleDamage", root, handleHummerDamage)
+
+
+function handlePDDamage(attacker, weapon, loss, x, y, z, tyreID)
+	if (weapon and getElementModel(source) == 470 or getElementModel(source) == 528 or getElementModel(source) == 596 or getElementModel(source) == 597 or getElementModel(source) == 598 or getElementModel(source) == 599 or getElementModel(source) == 490 or getElementModel(source) == 427 ) then
+		if (weaponsToDamageHummer[weapon]) then
+			setElementHealth(source, getElementHealth(source) + 22)
+		end
+	end
+end
+addEventHandler("onClientVehicleDamage", root, handlePDDamage)
